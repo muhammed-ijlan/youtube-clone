864e1647f0b62d112a18808eb471ae519a3e08f0
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-08-15 18:48:55 +0530

    test mvc working fine

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
+import userRoutes from "./routes/users.js"
+
 const app = express()
 dotenv.config()
-
-
-
 
 //MONGOOSE CONNECTION
 const connect = () => {
@@ -16,6 +15,9 @@ const connect = () => {
         throw err
     })
 }
+
+app.use("/api/users", userRoutes)
+
 
 //PORT LISTEN
 app.listen(8800, () => {
