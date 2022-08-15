77aef129d377b39a4f5c5f5ef996d01924c405ba
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-08-15 18:22:34 +0530

    user model created

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+const app = express()
+dotenv.config();
+
+
+
+
+
+
+//MONGOOSE CONNECTION
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connected TO DB")
+    }).catch((err) => {
+        throw err
+    })
+}
+
+//PORT LISTEN
+app.listen(8800, () => {
+    connect();
+    console.log("Server Connected!");
+}) 
