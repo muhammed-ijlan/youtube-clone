4021a844a6335df4f3b43432cc10705a0ef90f1f
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-08-16 11:25:54 +0530

    user routes added

---
+++ controllers/user.js
@@ -0,0 +1,24 @@
+// UPDATE USER
+export const updateUser = (req, res, next) => {
+
+}
+
+
+export const deleteUser = (req, res, next) => {
+
+}
+export const getUser = (req, res, next) => {
+
+}
+export const subscribe = (req, res, next) => {
+
+}
+export const unsubscribe = (req, res, next) => {
+
+}
+export const likeVideo = (req, res, next) => {
+
+}
+export const dislikeVideo = (req, res, next) => {
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,21 @@
 import express from "express"
-import { } from "../controllers/user.js"
+import { updateUser, deleteUser, dislikeVideo, getUser, likeVideo, subscribe, unsubscribe } from "../controllers/user.js"
 
 const router = express.Router();
+
+// UPDATE A USER
+router.put("/:id", updateUser)
+// DELETE A USER
+router.delete("/:id", updateUser)
+// GET A USER
+router.get("/find/:id", getUser)
+// SUBSCRIBE A USER
+router.put("/sub/:id", subscribe)
+// UNSUBSCRIBE A USER
+router.put("/unsub/:id", unsubscribe)
+// LIKE A VIDEO 
+router.put("/like/:videoId", likeVideo)
+// DISLIKE A VIDEO
+router.put("/dislike/:videoId", dislikeVideo)
 
 export default router;
