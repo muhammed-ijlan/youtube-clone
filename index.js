2c3e37e5bbe78187674acde7c31410ff0b2fe237
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-08-18 22:04:49 +0530

    connected with frontend react

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import userRoutes from "./routes/users.js"
 import commentRoutes from "./routes/comments.js"
@@ -9,8 +10,10 @@ import videoRoutes from "./routes/videos.js"
 import authRoutes from "./routes/auth.js"
 
 const app = express()
+app.use(cors())
 dotenv.config()
 app.use(express.json())
+
 
 //MONGOOSE CONNECTION
 const connect = () => {
