eccfe292fb2703b7da66ede5653d27a1c9c92b2d
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-08-18 16:35:02 +0530

    comments route simple structure created

---
+++ controllers/comment.js
@@ -0,0 +1,26 @@
+// ADD
+export const addComment = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+// DELETE
+export const deleteComment = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+// GET
+export const getComment = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
+}
